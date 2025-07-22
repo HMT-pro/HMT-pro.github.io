65894750a7694e2334b2c848249f593550055a4a
Leslie1445 <1838436410@qq.com>
2025-07-22 19:00:55 +0800

    Site updated: 2025-07-22 19:00:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/22/My-New-Post/","2025/07/22/hello-world/"];function toRandomPost(){
+var posts=["2025/07/22/hello-world/","2025/07/22/My-New-Post/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
